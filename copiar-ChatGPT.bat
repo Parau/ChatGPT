19ee9f483773fce2722c2aea251280b6af6c8fc4
APRENDER.digital <info@aprender.digital>
2024-10-04 16:04:02 -0300

    para rodar novamente o action de build

--- copiar-ChatGPT.bat
+++ copiar-ChatGPT.bat
@@ -3,4 +3,4 @@ xcopy "E:\dev\ChatGPT\blog\*" ".\blog\" /E /I
 xcopy "E:\dev\ChatGPT\docs\*" ".\docs\" /E /I
 xcopy "E:\dev\ChatGPT\src\*" ".\src\" /E /I
 xcopy "E:\dev\ChatGPT\static\*" ".\static\" /E /I
-copy "E:\dev\ChatGPT\*.*" ".\"
+copy "E:\dev\ChatGPT\*.*" ".\" 
