f2396aa8a52bad5ff4281026c7125ae7b31a341b
APRENDER.digital <info@aprender.digital>
2025-01-21 15:55:42 -0300

    ultima versao estava errada

--- gerar-docs-curso.bat
+++ gerar-docs-curso.bat
@@ -37,14 +37,13 @@ rem Apaga o arquivo index.md no caso de uma recópia para não dar o erro de arq
 del ".\docs-curso\03 - paranaues\03 - exploracao\index.md"
 copy ".\docs-guia\construcao\estrutura\*.*" ".\docs-curso\03 - paranaues\03 - exploracao"
 ren ".\docs-curso\03 - paranaues\03 - exploracao\exploracao.md" "index.md"
-rem Altera título do texto
-C:\ProgramData\miniconda3\python gerar-docs-curso-troca-texto.py ".\docs-curso\03 - paranaues\03 - exploracao\index.md" "# Exploração" "# Estrutura do prompt"
 rem Apagua trecho no final do texto 
 C:\ProgramData\miniconda3\python gerar-docs-curso-troca-texto.py ".\docs-curso\03 - paranaues\03 - exploracao\index.md" "<guia01>" "##excluir##" TAG
 rem Altera posição no sidebar
 C:\ProgramData\miniconda3\python gerar-docs-curso-troca-texto.py ".\docs-curso\03 - paranaues\03 - exploracao\index.md" "sidebar_position: 1" "sidebar_position: 5"
 
 
+*******************ESTOU RETIRANDO DO CURSO O ORÁCULO
 rem **** Oráculo ****
 md ".\docs-curso\03 - paranaues\04 - oraculo"
 rem Apaga o arquivo index.md no caso de uma recópia para não dar o erro de arquivo já existente
